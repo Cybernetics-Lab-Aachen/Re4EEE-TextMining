46bc5299c2ba845d6e292230950316dbb6680ba3
Chris Bohlman <cbohlmanaz@gmail.com>
2018-05-23 11:17:29 +0200

    Updated comments

--- readTweets.py
+++ readTweets.py
@@ -1,7 +1,7 @@
 # TODO: Get JSON directly from web
-# TODO: Check with Thorsten about deleting any URLs to other tweets (deleting anything after https)
-# TODO: Should I replace any other punctuation besides hyphens?
-# TODO: Ordering of preprocessing and statistics?
+# TODO: Deleting any URLs to other tweets (deleting anything after https)
+# TODO: Replace any other punctuation besides hyphens
+# TODO: Ordering of preprocessing and statistics
 
 import urllib.request               # lib that handles the url code
 import json                         # lib for json code
