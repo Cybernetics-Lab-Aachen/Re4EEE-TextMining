ac1dd780a058c79ca31a624a92a5c6b3920dba43
Devin Johnson <devinj1121@gmail.com>
2018-05-23 10:43:32 +0200

    final touches on wikiclean, open to improvements though

--- cleanwiki.py
+++ cleanwiki.py
@@ -9,7 +9,8 @@ from nltk.stem import LancasterStemmer, WordNetLemmatizer
 # Remove brackets, punctuation etc.
 def denoise(sample):
     index = sample.find("Text")
-    sample = sample[index:].lower()
+    sample = sample[index+4:].lower()
+    sample = re.sub("\\\\n", " ", sample)
     return re.sub("[^a-z ]", " ", sample)
 
 
@@ -32,24 +33,45 @@ def replace_nums(list):
 
 
 # Lemmatize verbs
-# TODO not working
-def lemmatize_verbs(words):
+def lemmatize(words):
     lemmatizer = WordNetLemmatizer()
     lemmas = []
     for word in words:
-        lemma = lemmatizer.lemmatize(word, 'v')
+        lemma = lemmatizer.lemmatize(word)
         lemmas.append(lemma)
     return lemmas
 
 
 # Replace unecessary words
-def remove_unecessary(words):
+def remove_stops(words):
     english_words = set(nltk.corpus.words.words())
     stops = set(stopwords.words("english"))
     filtered = []
     for word in words:
         if word not in stops and word in english_words and len(word) != 1:
             filtered.append(word)
+    return filtered
+
+
+# Get word count for each word
+def word_count(words):
+    dictionary = dict()
+    for word in words:
+        if word in dictionary:
+            dictionary[word] += 1
+        else:
+            dictionary[word] = 1
+    return dictionary
+
+
+# Remove low frequency words with little effect on results
+def remove_low_frequency(words):
+    counts = word_count(words)
+    items = counts.items()
+    filtered = []
+    for item in items:
+        if item[0] in counts and counts[item[0]] != 1:
+            filtered.append(item[0])
     return filtered
 
 
@@ -61,10 +83,12 @@ text = file.read()
 text = denoise(text)
 text = replace_contractions(text)
 
-# Tokenize, normalize
+# Tokenize, normalize, lemmatize, remove low frequency words
 words = nltk.word_tokenize(text)
 words = replace_nums(words)
-words = remove_unecessary(words)
+words = remove_stops(words)
+words = lemmatize(words)
+words = remove_low_frequency(words)
 print(words)
 
 
