28dd1773634e273bb2192fa657fbfac1a701441c
Devin Johnson <devinj1121@gmail.com>
2018-06-05 11:43:10 +0200

    fixed download bug

--- DownloadArticles.py
+++ DownloadArticles.py
@@ -19,7 +19,7 @@ start_time = time.clock()
 
 # Download relevant texts
 rand = randint(1, 18458000)
-for j in range(rand, rand + 1000):
+for i in range(rand, rand + 1000):
     title = lines[i].split(":")[2].strip("\n")
     if not any(title in file for file in os.listdir(".\\files_of_interest")):
         webpage = requests.get("http://triton.zlw-ima.rwth-aachen.de:50001/wikipedia/getArticleByTitle?title=" + urllib.parse.quote_plus(title)).content
