48ed09cf481092ad02212dbfea1240d120673122
Devin Johnson <devinj1121@gmail.com>
2018-06-15 15:18:44 +0200

    updates

--- wikipedia_files/DownloadArticles.py
+++ wikipedia_files/DownloadArticles.py
@@ -13,20 +13,18 @@ start_time = time.clock()
 count = 0
 
 # Download relevant texts
-rand = randint(1, 18458000)
-for i in range(rand, rand + 1000):
-    if count > 1000:
+rand = randint(1, 1841800)
+for i in range(rand, rand + 5000):
+    if count > 5000:
         exit(0)
     title = lines[i].split(":")[2].strip("\n")
     if not any(title in file for file in os.listdir("..\\sample_set")):
         webpage = requests.get("http://triton.zlw-ima.rwth-aachen.de:50001/wikipedia/getArticleByTitle?title=" + urllib.parse.quote_plus(title)).content
         soup = bs4.BeautifulSoup(webpage, "lxml")
         text = soup.getText().lower()
-        if ('learn' in text or 'educat' in text) and ('teach' in text or 'student' in text) \
-                and ('method' in text or 'tool' in text or 'concept' in text or 'platform' in text or 'tech' in text):
-            file = open('..\\sample_set\\' + re.sub("[^\w\d]", " ", title, re.UNICODE) + '.txt', 'w', encoding='utf-8')
-            file.write(text)
-            file.close()
+        file = open('..\\sample_set\\' + re.sub("[^\w\d]", " ", title, re.UNICODE) + '.txt', 'w', encoding='utf-8')
+        file.write(text)
+        file.close()
     count += 1
     print(time.clock() - start_time, "seconds")
 
@@ -35,4 +33,3 @@ for i in range(rand, rand + 1000):
 # Third runtime: 111.41879792404077 seconds
 # Fourth runtime: 111.0834045599835 seconds
 # Fifth runtime: 98.12181727716322 seconds
->>>>>>> 0d932cc38b4ac73f5e8336d91e884224567c7d03
